e0ef25a51f7bba24d701f2dae65f8f8e7f73140f
yasmin0715 <129914162+yasmin0715@users.noreply.github.com>
2023-05-16 10:21:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+pizzaListArray = [];
+
+function getmenu()
+	{    
+        // Dê um nome apropriado ao id como pizzaName
+        var item = document.getElementById("pizzaName").value; 
+       
+        //Adicione a função push() para empurrar itens dentro para da array
+        pizzaListArray.push(pizzaListArray)(item);
+        
+        var removeCommas = pizzaListArray.join("<br># ");
+        document.getElementById("displayMenu").innerHTML =  "# " + removeCommas;
+	}
